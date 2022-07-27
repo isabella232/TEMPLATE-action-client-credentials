978eefbab29a1a078bba0e3866c6122e71832bd3
Josh Cunningham <josh@joshcanhelp.com>
2022-07-26 20:30:36 -0700

    chore: update docker image; formatting

--- integration/integration.action.spec.js
+++ integration/integration.action.spec.js
@@ -4,7 +4,8 @@ const { apiMock } = require("../__mocks__/api-client-credentials");
 const { onExecuteCredentialsExchange } = require("./integration.action");
 
 describe("Action integration", () => {
-  let consoleLogMock, eventMock;
+  let consoleLogMock;
+  let eventMock;
 
   beforeEach(() => {
     consoleLogMock = jest.spyOn(console, "log").mockImplementation();
